81e6185c46fcf60a64cf11c1cc5bee5cc806f278
Adithya <adhi56822@gmail.com>
2026-05-01 00:00:20 +0530

    Fix remaining three.js imports

--- public/snowfall.js
+++ public/snowfall.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "https://unpkg.com/three@0.164.1/build/three.module.js";
 
 // Create leaf snowfall effect that integrates with existing scene and camera
 export function createLeafSnowfall(scene, camera, count = 50) {
