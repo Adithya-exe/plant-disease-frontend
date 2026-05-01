224a19297f5fd9072a561591dd3158a29d8b4508
Adithya <adhi56822@gmail.com>
2026-05-01 16:29:26 +0530

    final remedy debugging

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 import * as THREE from "https://unpkg.com/three@0.164.1/build/three.module.js";
-import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.164.1/examples/jsm/controls/OrbitControls.js";
+import { OrbitControls } from "https://unpkg.com/three@0.164.1/examples/jsm/controls/OrbitControls.js";
 import { createButtonFireflyLayer } from './fireflies.js';
 import { createLeafSnowfall } from './snowfall.js';
 
