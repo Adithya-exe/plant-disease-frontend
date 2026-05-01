c8895fb65352893cfa62581df212dac672bcae1f
Adithya <adhi56822@gmail.com>
2026-05-01 16:19:58 +0530

    final remedy inclusion

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.164.1/build/three.module.js";
+import * as THREE from "https://unpkg.com/three@0.164.1/build/three.module.js";
 import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.164.1/examples/jsm/controls/OrbitControls.js";
 import { createButtonFireflyLayer } from './fireflies.js';
 import { createLeafSnowfall } from './snowfall.js';
